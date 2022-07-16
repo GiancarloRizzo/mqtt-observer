566c7bb8852a2e5c8aba97496d51eda07424d93e
Rizzo, Giancarlo <giancarlo.rizzo@dlr.de>
2022-07-16 12:19:45 +0200

    add params for mqtt and metric-server

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+# mqtt-defaults
+broker = localhost
+port   = 1883
+qos    = 2
 topics = [
     "topic/1",
     "topic/2",
 ]
+
+
+# scrape-defaults for prometheus
+scrape_destination = 'localhost' 
+scrape_port = 9630
+scrape_url = '/mqtt_metrics'
